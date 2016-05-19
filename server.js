f6ca49f9af61840da09584c0f6e0ea2f2e2b202e
gtrujillop <gaston.trujillo.java@gmail.com>
2016-05-18 22:08:11 -0500

    Fix app default index in server.js.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/components'));
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get('/#/people', function(req, res) {
 
     // make sure index is in the right directory. In this case /app/index.html
     res.render('index');
